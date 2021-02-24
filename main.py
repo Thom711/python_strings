e513bef1ab42caeea1e2938f39ed18edd527940a
Thom711 <tvdveldt@hotmail.com>
2021-02-24 16:07:32 +0100

    v2, abstracted part2

--- main.py
+++ main.py
@@ -13,17 +13,18 @@ goal_1_player = "Marco van Basten"
 
 scorers = goal_0_player + ' ' + str(goal_0) + ', ' + goal_1_player + ' ' + str(goal_1)
 
-report = f"{goal_0_player} scored in the {str(goal_0)}nd minute \n{goal_1_player} scored in the {str(goal_1)}th minute"
+report = f"{goal_0_player} scored in the {goal_0}nd minute \n{goal_1_player} scored in the {goal_1}th minute"
+
 
 # PART 2
 player = "Ronald Koeman"
 
-first_name = player[player.find("Ronald"):len("Ronald")]
+first_name = player[:player.find(' ')]
 
-last_name_len = len(player[player.find("Koeman"):])
+last_name_len = len(player[player.find(' ')+1:])
 
-name_short = player[player.find("Ronald"):1] + '. ' + player[player.find("Koeman"):]
+name_short = player[0].upper() + '. ' + player[player.find(' ')+1:]
 
-chant = ((player[player.find("Ronald"):len("Ronald")] + '! ') * 6).strip() 
+chant = ((player[:player.find(' ')] + '! ') * len(player[:player.find(' ')])).strip()
 
 good_chant = chant[-1] != ' '
